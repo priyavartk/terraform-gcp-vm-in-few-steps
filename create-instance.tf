8584fcb349d5fbbf20d7a75596febc6819346051
priyavart kaushik <priyavartk@gmail.com>
2023-08-28 12:30:35 +0100

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "default" {
   name         = "virtual-machine-from-terraform"
   machine_type = "f1-micro"
-  zone         = "us-central1-a"
+  zone         = "europe-east1"
 
   boot_disk {
     initialize_params {
@@ -21,20 +21,6 @@ resource "google_compute_instance" "default" {
 
     // Apply the firewall rule to allow external IPs to access this instance
     tags = ["http-server"]
-}
-
-resource "google_compute_firewall" "http-server" {
-  name    = "default-allow-http-terraform"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  // Allow traffic from everywhere to instances with an http-server tag
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["http-server"]
 }
 
 output "ip" {
