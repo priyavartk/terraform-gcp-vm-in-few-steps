8cbde2889dd66aee71a6a9361ca5c605d2629555
priyavart kaushik <priyavartk@gmail.com>
2023-08-28 12:45:29 +0100

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -1,11 +1,11 @@
 resource "google_compute_instance" "default" {
   name         = "virtual-machine-from-terraform"
-  machine_type = "f1-micro"
-  zone         = "europe-west1-b"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-c"
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1604-lts"
+      image = "centos-cloud/centos-7"
     }
   }
 
@@ -17,7 +17,7 @@ resource "google_compute_instance" "default" {
     }
   }
 
-    metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
+    metadata_startup_script = "sudo yum update && sudo yum install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
 
     // Apply the firewall rule to allow external IPs to access this instance
     tags = ["http-server"]
